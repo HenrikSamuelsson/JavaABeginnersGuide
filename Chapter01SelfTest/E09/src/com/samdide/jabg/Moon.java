f2e16083e3336c85a1a84d760abed8176b00a958
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-06-12 12:42:15 +0200

    Committing exercise 9.

---
+++ Chapter01SelfTest/E09/src/com/samdide/jabg/Moon.java
@@ -0,0 +1,22 @@
+package com.samdide.jabg;
+
+import java.text.DecimalFormat;
+
+/**
+ * Converts a weight on earth to the corresponding weighth on the moon.
+ * 
+ * @author Henrik Samuelsson
+ */
+public class Moon {
+	public static void main(String[] args) {
+		double weightOnEarth = 86.2;
+		double conversionFactor = 0.17;
+		double weightOnMoon;
+
+		weightOnMoon = weightOnEarth * conversionFactor;
+		DecimalFormat numberFormat = new DecimalFormat("#.0");
+		System.out.println(numberFormat.format(weightOnEarth)
+				+ " kg on the earth corresponds to "
+				+ numberFormat.format(weightOnMoon) + " kg on the moon.");
+	}
+}
