f67df1845594935d5c19d5a2810fa3f57fd5aeef
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-06-28 20:11:55 +0200

    Committing solution for selftest 10.

---
+++ Chapter03SelfTest/E10/ChangeCase/src/changecase/ChangeCase.java
@@ -0,0 +1,38 @@
+package changecase;
+
+/**
+ * Changes lower case characters to upper and vice versa.
+ * Supports the swedish letters å, ä and ö in addition to the letters in the
+ * english alphabet.
+ * 
+ * @author Henrik Samuelsson
+ */
+public class ChangeCase {
+
+    /**
+     * Entry point to application.
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws java.io.IOException {
+        char c;
+        int changeCount = 0;
+       
+        System.out.println("Enter period to stop.");
+        
+        do {
+            c = (char) System.in.read();
+            if((c >= 'a' & c <= 'z' ) || (c == 'å') || (c == 'ä') || (c == 'ö')) {
+                c -= 32;
+                changeCount++;
+                System.out.print(c);
+            }
+            else if((c >= 'A' & c <= 'Z' )  || (c == 'Å') || (c == 'Ä') || (c == 'Ö')) {
+                c += 32;
+                changeCount++;
+                System.out.print(c);
+            }
+        } while(c != '.');
+        System.out.println("\nChangend " + changeCount + " letters.");
+    }
+}
