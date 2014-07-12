341d054029f8c0e8cd832e4ff03c47587da0b715
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-07-12 23:20:55 +0200

    Committing Try This 8-1

---
+++ TryThis/08-01/src/pkg08/pkg01/ICharQ.java
@@ -0,0 +1,21 @@
+package pkg08.pkg01;
+
+/**
+ * A charcater queue interface.
+ */
+public interface ICharQ {
+    /**
+     * Put a character at the end of the queue.
+     * 
+     * @param ch character that will go into the queue
+     */
+    void put(char ch);
+    
+    /**
+     * Get a character from the front of the queue.
+     * 
+     * @return one character from the queue
+     */
+    char get();
+    
+}
