564aae8e6b15acf652a9398e62661c67eeb0b591
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-08-09 20:04:55 +0200

    Committing exercise 10.

---
+++ Chapter17SelfTest/E10/JavoFXEventDemo/src/javofxeventdemo/JavoFXEventDemo.java
@@ -0,0 +1,53 @@
+package javofxeventdemo;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+/**
+ * Demonstrates JavaFX events and buttons.
+ */
+public class JavoFXEventDemo extends Application {
+    Label response;
+    
+     /**
+     * Entry point to application.
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Use JavaFX Buttons and Events.");
+        FlowPane rootNode = new FlowPane(10, 10);
+        rootNode.setAlignment(Pos.CENTER);
+        Scene myScene = new Scene(rootNode, 300, 100);
+        primaryStage.setScene(myScene);
+        response = new Label("Push a Button");
+        
+        // Setup the buttons.
+        Button btnUp = new Button("Run");
+        Button btnExit = new Button("Exit");
+        btnUp.setOnAction((ActionEvent ae) -> {
+            response.setText("You pressed Run");
+        });
+        btnExit.setOnAction((ActionEvent ae) -> {
+            Platform.exit();
+        });
+        
+        rootNode.getChildren().addAll(btnUp, btnExit, response);
+        primaryStage.show();
+    }
+
+   
+    
+}
