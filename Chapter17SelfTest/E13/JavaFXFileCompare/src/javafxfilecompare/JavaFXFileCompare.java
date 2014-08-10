9bd38a4dcf83d1b65f1f89ac12b6fc568e29b29d
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2014-08-10 14:47:51 +0200

    Committing Exercise 13, code cleanup.

--- Chapter17SelfTest/E13/JavaFXFileCompare/src/javafxfilecompare/JavaFXFileCompare.java
+++ Chapter17SelfTest/E13/JavaFXFileCompare/src/javafxfilecompare/JavaFXFileCompare.java
@@ -12,7 +12,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
